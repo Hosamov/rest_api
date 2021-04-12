41f9234de1400ae93a62962beb10d1e07415019c
Hosamov <hosamov@hotmail.com>
2021-04-12 14:10:06 -0700

    add courses routes

--- routes/index.js
+++ routes/index.js
@@ -27,28 +27,37 @@ router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
 */
 router.get('/courses', asyncHandler(async (req, res) => {
   let courses;
-  try {
-    //res.json('/api/courses GET route is working');
+  try {;
     courses = await Course.findAll();
     console.log(courses);
     res.status(200).json({courses})
   } catch (error) {
-    res.status(400).json( {error} );
+    res.status(400).json( { error } );
   }
 }));
 
 /* /api/courses/:id GET route
 // Returns a corresponding course along with the User that owns that course
 */
-router.get('/courses/:id',  asyncHandler(async (req, res) => {
-  res.json('/api/courses/:id GET route is working');
+router.get('/courses/:id',  asyncHandler(async (req, res, next) => {
+  //res.json('/api/courses/:id GET route is working');
+  try {
+    const course = await Course.findByPk(req.params.id);
+    if(course !== null) {
+      res.status(200).json( { course } );
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(404).json( { error: "Course Not Found" } );
+  }
 }));
 
 /* /api/users POST route
 // Creates a new user
 // redirects to '/'
 */
-router.post('/users', async (req, res) => {
+router.post('/users', asyncHandler(async (req, res) => {
   try {
     await User.create(req.body);
     res.status(201).json({ "message": "Account successfully created!" });
@@ -57,20 +66,28 @@ router.post('/users', async (req, res) => {
     console.log(error);
     if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
       const errors = error.errors.map(err => err.message);
-      res.status(400).json({ errors });
+      res.status(400).json( { errors } );
     } else {
       throw error;
     }
   }
-});
+}));
 
 /* /api/courses POST route
 // Creates a new course
 // Sets location header to URI for newly created course
 */
 router.post('/courses', asyncHandler(async (req, res) => {
-    res.json('/api/courses POST route is working!');
+  //res.json('/api/courses POST route is working!');
+  let course;
+  try {
+    course = await Course.create(req.body); //create a new course using req.body object
+    const courseId = course.id;
 
+    res.status(201).location('/courses/' + courseId).json("course created");
+  } catch (error) {
+    console.log(error);
+  }
 }));
 
 /* /api/courses/:id PUT route
